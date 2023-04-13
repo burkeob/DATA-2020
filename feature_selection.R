2078f712f9dfa6d2efbc8985b2ac5356b6060838
Brad Marx <bradmarx112@gmail.com>
2023-04-12 20:49:53 -0400

    Fwd Bck regression and Ttest in loop

--- feature_selection.R
+++ feature_selection.R
@@ -10,75 +10,50 @@ library(survey)
 
 # setwd("~/Desktop/DSI Spring/Stats/Final Project")
 
-
 ####################################################
 # TODO: Get regression for each variable in a loop #
 ####################################################
 
 load('cleaned_data.Rdata')
 
+df_covariates <- df %>% dplyr::select(year, wrkstat, marital, age, race, educ, sex, born, income,
+                                      region, partyid, relig, zodiac, vstrat, vpsu, wgt_comb)
 
-df <- df %>% dplyr::select(race_imp, year, wrkstat, marital, age, race, educ, sex, born, income,
-                            region, partyid, relig, zodiac, vstrat, vpsu, wgt_comb) %>% drop_na()
+df_variables <- df %>% dplyr::select(wealth_imp, sex_imp, parents_imp, educ_imp, hardWork_imp, rightPpl_imp,
+                                     political_imp, race_imp, religion_imp)
 
-df <- df %>% filter(race != '.i:  Inapplicable')
-
-surv.des <- svydesign(data = df, ids = ~vpsu, weights = ~wgt_comb, strata = ~vstrat, nest=TRUE)
-
-# T-Tests between year and every variable
-
-svyttest(formula = wealth_imp ~ year, design = surv.des)
-svyttest(formula = sex_imp ~ year, design = surv.des)
-svyttest(formula = parents_imp ~ year, design = surv.des)
-svyttest(formula = educ_imp ~ year, design = surv.des)
-svyttest(formula = hardWork_imp ~ year, design = surv.des)
-svyttest(formula = rightPpl_imp ~ year, design = surv.des)
-svyttest(formula = political_imp ~ year, design = surv.des)
-svyttest(formula = race_imp ~ year, design = surv.des)
-svyttest(formula = religion_imp ~ year, design = surv.des)
-
+stat_matrix <- matrix(NA, 9, 1)
+fwd_mdl_list <- list()
+bck_mdl_list <- list()
+idx <- 1
 # (WIP) Forwards and Backwards Logistic Regression
-
-educ_full <- svyglm(formula = educ_imp~year+wrkstat+marital+age+race+educ+sex+born+income
-       +region+partyid+relig+zodiac, 
-       design = surv.des, family=quasibinomial(link="logit"))
-
-educ_none <- svyglm(formula = educ_imp~1, design = surv.des, family=quasibinomial(link="logit"), data = df)
-
-
-# Using AIC as metric for feature selection
-fwd_step_mdl <- stepAIC(object = educ_none, direction='forward', scope=list(upper=educ_full, lower=educ_none))
-summary(fwd_step_mdl)
-
-# Using AIC as metric for feature selection
-bkd_step_edu <- stepAIC(educ_full, direction='backward')
-summary(bkd_step_edu)
-
-
-wealth_full <- svyglm(formula = wealth_imp~year+wrkstat+marital+age+race+educ+sex+born+income
+for (col in df_variables){
+  # Create data we need for specific variable
+  df_cov_temp <- df_covariates
+  df_cov_temp$var <- col
+  df_cov_temp <- df_cov_temp %>% drop_na()
+  
+  # Create Survey design object to control weights
+  surv.des <- svydesign(data = df_cov_temp, ids = ~vpsu, weights = ~wgt_comb, strata = ~vstrat, nest=TRUE)
+  
+  # t-test between year and variable
+  ttest.svy <- svyttest(formula = var ~ year, design = surv.des)
+  stat_matrix[idx] <- ttest.svy$p.value
+  
+  # Forward and Backward stepwise regression
+  lm_full <- svyglm(formula = var ~ year+wrkstat+marital+age+race+educ+sex+born+income
                     +region+partyid+relig+zodiac, 
                     design = surv.des, family=quasibinomial(link="logit"))
-
-wealth_none <- svyglm(formula = wealth_imp~1, design = surv.des, family=quasibinomial(link="logit"), data = df)
-
-
-bkd_step_wlth <- stepAIC(wealth_full, direction='backward')
-summary(bkd_step_wlth)
-
-fwd_step_wlth <- stepAIC(object = wealth_none, direction='forward', scope=list(upper=wealth_full, lower=wealth_none))
-summary(fwd_step_wlth)
-
-
-race_full <- svyglm(formula = race_imp~year+wrkstat+marital+age+race+educ+sex+born+income
-                      +region+partyid+relig+zodiac, 
-                      design = surv.des, family=quasibinomial(link="logit"))
-
-race_none <- svyglm(formula = race_imp~1, design = surv.des, family=quasibinomial(link="logit"), data = df)
-
-bkd_step_race <- stepAIC(race_full, direction='backward')
-summary(bkd_step_race)
+  lm_none <- svyglm(formula = var~1, design = surv.des, family=quasibinomial(link="logit"), data = df_cov_temp)
+  
+  fwd_step_mdl <- stepAIC(object = lm_none, direction='forward', scope=list(upper=lm_full, lower=lm_none))
+  fwd_mdl_list <- append(fwd_mdl_list, list(summary(fwd_step_mdl)))
+  
+  # Using AIC as metric for feature selection
+  bkd_step_mdl <- stepAIC(lm_full, direction='backward')
+  bck_mdl_list <- append(bck_mdl_list, list(summary(bkd_step_mdl)))
+  
+  idx <- idx + 1
+}
 
 
-# Using AIC as metric for feature selection
-fwd_step_race <- stepAIC(object = race_none, direction='forward', scope=list(upper=race_full, lower=race_none))
-summary(fwd_step_race)
